f33fe4c28552e7f262be083f4f78651ba038e41c
Maria Beatriz S. Jocson <mbsjocson@headoffice.corp.bpi.com.ph>
2026-01-13 20:08:53 +0800

    commit for M4_Activity7

---
+++ src/com/bea/test/InvalidAmountException.java
@@ -0,0 +1,14 @@
+package com.bea.test;
+
+final class InvalidAmountException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -2789521198925483077L;
+
+	public InvalidAmountException(String message) {
+		super(message);
+	}
+	
+}
